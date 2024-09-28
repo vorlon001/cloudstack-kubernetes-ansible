be982a4ae9a2f9478ae62faee14d4829472f2e1a
KOSH VORLON <vorlon@iblog.pro>
2024-09-28 18:03:12 +0500

    refactory k8s 1.31.1 v116

---
+++ run-kubernetes-node3456.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+function throw()
+{
+   errorCode=$?
+   echo "Error: ($?) LINENO:$1"
+   exit $errorCode
+}
+
+function check_error {
+  if [ $? -ne 0 ]; then
+    echo "Error: ($?) LINENO:$1"
+    exit 1
+  fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+ansible-galaxy collection install community.kubernetes || throw ${LINENO}
+
+ansible  -i node-hosts-all.yaml --become -m shell -a 'uptime' kubernetes || throw ${LINENO}
+
+ANSIBLE_LOG_PATH=./install-kubebernetes-node56.log ansible-playbook -i node-hosts-all-kube-node3-4-5-6.yaml playbook-kubernetes/kubernetes-deploy.yaml --extra-vars "@run-kubernetes-node3456.vars.yaml" || throw ${LINENO}
+ANSIBLE_LOG_PATH=./post-install-kubebernetes-node56.log ansible-playbook -i node-hosts-all-kube-node3-4-5-6.yaml playbook-kubernetes/kubernetes-post-deploy.yaml --extra-vars "@run-kubernetes-node3456.vars.yaml" || throw ${LINENO}
+
